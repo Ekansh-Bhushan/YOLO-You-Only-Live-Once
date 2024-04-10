cd2746b28f3deab0056ad6479dd55647e60c2f67
Maanit <maanitarora1234@gmail.com>
2024-04-10 16:38:56 +0530

    Webpage Draft #1

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-def HelloWorld(a):
-    print(a)
-    
-HelloWorld("print")
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:manu1234@server/db'
+db = SQLAlchemy(app)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'manu1234'
+app.config['MYSQL_DB'] = 'MyDB'
+
+mysql = MySQL(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == "POST":
+        details = request.form
+        firstName = details['fname']
+        lastName = details['lname']
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
