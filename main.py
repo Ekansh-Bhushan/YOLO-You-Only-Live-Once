898d47e5726a53ceae4b8faba39f13a2f120584d
Maanit <maanitarora1234@gmail.com>
2024-04-11 00:15:19 +0530

    User creation page with flask mysql

--- main.py
+++ main.py
@@ -1,31 +1,25 @@
 from flask import Flask, render_template,request
-from flask_sqlalchemy import SQLAlchemy
 from flask_mysqldb import MySQL
 
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:manu1234@server/db'
-db = SQLAlchemy(app)
-
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'manu1234'
-app.config['MYSQL_DB'] = 'MyDB'
+app.config['MYSQL_DB'] = 'users_db'
 
 mysql = MySQL(app)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
-        details = request.form
-        firstName = details['fname']
-        lastName = details['lname']
+        name = request.form['name']
+        phone=request.form['phone']
+        email=request.form['email']
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
+        cur.execute("INSERT INTO users_db(name,phone,email) VALUES (%s, %s,%s)", (name,phone,email))
         mysql.connection.commit()
         cur.close()
-        return 'success'
+        return "success"
     return render_template('index.html')
 
 if __name__ == "__main__":
