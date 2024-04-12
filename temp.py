a046a95e40a4f9e614d9875013a5412df63e308a
Ekansh <129383527+Ekansh-Bhushan@users.noreply.github.com>
2024-04-13 02:15:15 +0530

    ok

--- temp.py
+++ temp.py
@@ -2,7 +2,7 @@ import pytesseract as tess
 from PIL import Image
 tess.pytesseract.tesseract_cmd = r'C:/Program Files/Tesseract-OCR/tesseract.exe'
 
-img =Image.open(r"pres.jpg")
+img =Image.open(r"C:/Users/Ekansh/code/vihaan007/sample2.jpg")
 text=list(tess.image_to_string(img).splitlines())
 
 print(text)
