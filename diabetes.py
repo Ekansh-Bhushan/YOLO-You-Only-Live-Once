0aa5b6678094341a275e308e5c008d2c0aa0c952
Maanit <maanitarora1234@gmail.com>
2024-04-13 02:13:41 +0530

    intial

---
+++ diabetes.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+from pandas import read_csv
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+import graphviz 
+from sklearn import model_selection
+from sklearn.preprocessing import Imputer
+from sklearn.model_selection import train_test_split, cross_val_score, KFold, learning_curve, StratifiedKFold, train_test_split
+from sklearn.metrics import confusion_matrix, make_scorer, accuracy_score 
+from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import xgboost as xgb
+from xgboost import XGBClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, VotingClassifier
+from sklearn.neural_network import MLPClassifier as MLPC
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+import warnings
+warnings.filterwarnings("ignore")
+%matplotlib inline
--- main.py
+++ main.py
@@ -1,65 +1,59 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
-from random import choice as r_c
 
 app = Flask(__name__)
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER1'] = 'root'
-app = Flask(__name__)
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'manu1234'
 
 # Configuration for users database
 app.config['MYSQL_USERS_HOST'] = 'localhost'
+app.config['MYSQL_USERS_USER'] = 'root'
+app.config['MYSQL_USERS_PASSWORD'] = 'manu1234'
 app.config['MYSQL_USERS_DB'] = 'users_db'
 
 # Configuration for doctors database
-app.config['MYSQL_DOCTORS_HOST'] = 'localhost'
-app.config['MYSQL_DOCTORS_DB'] = 'doctors_db'
+# app.config['MYSQL_DOCTORS_HOST'] = 'localhost'
+# app.config['MYSQL_DOCTORS_USER'] = 'root'
+# app.config['MYSQL_DOCTORS_PASSWORD'] = 'manu1234'
+# app.config['MYSQL_DOCTORS_DB'] = 'doctors_db'
 
 mysql_users = MySQL(app)
-mysql_doctors = MySQL(app)
-
-mysql_users = MySQL(app)
-mysql_doctors = MySQL(app)
+# mysql_doctors = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
+        # Access users database
+        cur = mysql_users.connection.cursor()
         username = request.form['username']
         firstname = request.form['firstname']
         lastname = request.form['lastname']
-        phone=request.form['phone']
-        email=request.form['email']
-        # dob1=request.form['dob1']
-        cur = mysql_users.connection.cursor() 
+        phone = request.form['phone']
+        email = request.form['email']
 
-        cur.execute("INSERT INTO users_db(username,firstname,lastname,phone,email) VALUES (%s, %s,%s,%s,%s)", (username,firstname,lastname,phone,email,))
+        cur.execute("INSERT INTO users_db(username, firstname, lastname, phone, email) VALUES (%s, %s, %s, %s, %s)",
+                    (username, firstname, lastname, phone, email))
         mysql_users.connection.commit()
         cur.close()
-        return "success"
+        return "User registration success"
     return render_template('index.html')
 
-@app.route('/doctor_reg', methods=['GET', 'POST'])
-def doctors():
-    if request.method == "POST":
-        username = request.form['username']
-        firstname = request.form['firstname']
-        lastname = request.form['lastname']
-        phone=request.form['phone']
-        email=request.form['email']
-        spec=request.form['spec']
+# @app.route('/doctor_reg', methods=['GET', 'POST'])
+# def doctors():
+#     if request.method == "POST":
+#         # Access doctors database
+#         cur = mysql_doctors.connection.cursor()
+#         username = request.form['username']
+#         firstname = request.form['firstname']
+#         lastname = request.form['lastname']
+#         phone = request.form['phone']
+#         email = request.form['email']
+#         spec = request.form['spec']
         
-        
-        cur1 = mysql_doctors.connection.cursor() 
-        
-        cur1.execute("INSERT INTO doctors_db(username,firstname,lastname,phone,email,spec) VALUES (%s, %s,%s,%s,%s,%s)", (username,firstname,lastname,phone,email,spec))
-        # SEED = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_1234567890#"
-
-        mysql_doctors.connection.commit()
-        cur1.close()
-        return "success"
-    return render_template('doctor_reg.html')
+#         cur.execute("INSERT INTO doctors_db(username, firstname, lastname, phone, email, spec) VALUES (%s, %s, %s, %s, %s, %s)",
+#                     (username, firstname, lastname, phone, email, spec))
+#         mysql_doctors.connection.commit()
+#         cur.close()
+#         return "Doctor registration success"
+#     return render_template('doctor_reg.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
